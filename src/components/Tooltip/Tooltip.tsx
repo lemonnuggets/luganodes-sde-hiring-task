4e24ff7217cd8c63b4856e88412fa435c23bd81a
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2023-08-02 08:12:03 +0530

    feat: better tooltips

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -5,8 +5,8 @@ type Props = {
 };
 const Tooltip = ({ content, children }: Props) => {
   return (
-    <div className={`${styles.tooltipWrapper} w-fit`}>
-      <div className={styles.tooltip}>{content}</div>
+    <div className={`${styles.tooltipWrapper} w-fit`} data-title={content}>
+      {/* <div className={`${styles.tooltip} rounded-sm px-2`}>{content}</div> */}
       {children}
     </div>
   );
